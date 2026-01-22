d8aff38fd4e239b834ec64ef7568b617166c905e
praveendigitalks <praveendigitalks@gmail.com>
2026-01-22 23:17:20 +0530

    login success

--- environment/enviornment.ts
+++ environment/enviornment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
-    baseurl: 'http://localhost:5000/sp', 
- // baseurl : "https://praveensrivastav-backend.onrender.com/sp"
+    // baseurl: 'http://localhost:5000/sp', 
+ baseurl : "https://praveensrivastav-backend.onrender.com/sp"
 
 }
