f270bedb2da18aaa227e9695fa19ef407a87204b
praveendigitalks <praveendigitalks@gmail.com>
2026-01-24 17:49:49 +0530

    added uplaod image multer

--- src/app/authentication/interceptor/auth.interceptor.ts
+++ src/app/authentication/interceptor/auth.interceptor.ts
@@ -1,18 +1,25 @@
-  import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 
-  export const authInterceptor: HttpInterceptorFn = (req, next) => {
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('profileToken');
+  const deviceId = localStorage.getItem('deviceId');
 
-   const token = localStorage.getItem('profileToken');
-   const deviceId = localStorage.getItem('deviceId');
+  // List of URLs to skip the interceptor
+  const skipUrls = [
+    '/about'  // add relative path from your environment.baseurl
+  ];
 
-   if(token && deviceId){
+  // Check if the request URL includes any of the skip URLs
+  const shouldSkip = skipUrls.some(url => req.url.includes(url));
+
+  if (!shouldSkip && token && deviceId) {
     req = req.clone({
-      setHeaders : {
-        Authorization : `Bearer ${token}`,
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
         'x-device-id': deviceId,
       }
-    })
-   }
+    });
+  }
 
-    return next(req);
-  };
+  return next(req);
+};
